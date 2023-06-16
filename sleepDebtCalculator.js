632888d38e3388280047513d723da8b3c80d18bf
Megan Kallis <megankallis@gmail.com>
2023-06-15 18:57:30 -0700

    Determines how many hours a user slept based on what day is input

---
+++ sleepDebtCalculator.js
@@ -0,0 +1,33 @@
+// This calculator determines the actual and ideal hours of sleep for each night of the last week
+// It also calculates, in hours, how far you are from your weekly sleep goal
+
+// Returns the amount of hours you slept on a given day
+const getSleepHours = day => {
+    switch (day) {
+        case 'monday':
+            return 8;
+            break;
+        case 'tuesday':
+            return 7;
+            break;
+        case 'wednesday':
+            return 6;
+            break;
+        case 'thursday':
+            return 7;
+            break;
+        case 'friday':
+            return 5;
+            break;
+        case 'saturday':
+            return '8';
+            break;
+        case 'sunday':
+            return '9';
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(getSleepHours('monday'));
